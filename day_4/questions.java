614fece909367565239724461443a42f1f99b565
vibha-sharma11 <99333960+vibha-sharma11@users.noreply.github.com>
2022-02-14 21:34:29 +0530

    Update questions.java

--- day_4/questions.java
+++ day_4/questions.java
@@ -7,9 +7,9 @@ public class questions {
     public static void main(String[] args) {
         // Q-- Reverse A String In Java
 
-        // Scanner in = new Scanner(System.in);
-        // System.out.print("Please enter the word you want to be reversed: ");
-        // String word = in.next();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter the word you want to be reversed: ");
+        String word = in.next();
         // String rev = "";
         // int i = word.length();
         // while (i > 0) {
@@ -20,23 +20,23 @@ public class questions {
 
         // Q-- HCF Of Two Numbers Program
 
-        // Scanner in = new Scanner(System.in);
-        // System.out.println("Please enter two numbers:");
-        // int num1 = in.nextInt();
-        // int num2 = in.nextInt();
-        // int factor = 2;
-        // int max = 2;
-        // while (num1 >= 2 && num2 >= 2) {
-        // if (num1 % factor == 0 && num2 % factor == 0) {
-        // if (factor > max ) {
-        // max = factor;
-        // }
-        // factor++;
-        // }else{factor++;}
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter two numbers:");
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        int factor = 2;
+        int max = 2;
+        while (num1 >= 2 && num2 >= 2) {
+        if (num1 % factor == 0 && num2 % factor == 0) {
+        if (factor > max ) {
+        max = factor;
+        }
+        factor++;
+        }else{factor++;}
 
-        // System.out.println(max);
-        // }
-        // in.close();
+        System.out.println(max);
+        }
+        in.close();
         //Q-- Abir is allowed to go out with his friends only on the even days of a given month. Write a program to count the number of days he can go out in the month of August.
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter your month: ");
